520de70ff22218f2535eb5c296e44aa2e47f7efd
Jowjokiyolo <Jowjokiyolo@gmail.com>
2025-05-08 20:30:09 +0200

    8pm commit (rare), but removed junk

--- maintenance_calculation.py
+++ maintenance_calculation.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from maintenance_structure import read_excel_to_df, structure_dataframe
-from filter_functions import filter_ac
 
 def daily_tasks(data: pd.DataFrame, years=5):
     maintenance_df = pd.DataFrame(columns=['Day', 'Day Tasks', 'Day Men', 'Day Hours'])
@@ -14,16 +12,5 @@ def daily_tasks(data: pd.DataFrame, years=5):
             if pd.notna(interval) and day % interval == 0:
                 day_tasks.extend(group['Task Number'].tolist())
 
-        if day_tasks:
-            maintenance_df = pd.concat(
-                [maintenance_df, pd.DataFrame({'Day': [day], 'Day Tasks': [day_tasks]})],
-                ignore_index=True
-            )
-    maintenance_df.to_csv(r"goon.csv")
-    return maintenance_df
 
-GOON = read_excel_to_df(r".Files/MPD.xlsx")
-STRUC = structure_dataframe(GOON)
-FILTER = filter_ac(STRUC)
-CUNNY = daily_tasks(FILTER)
-print(CUNNY)
+    return maintenance_df
